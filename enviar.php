9d895b3084ce0f5dc6e42819a8d3f1b7359f5ce3
DVargas18 <dvargas@grupoptm.com>
2019-02-22 19:30:23 -0600

    cambio Correo

---
+++ enviar.php
@@ -0,0 +1,22 @@
+﻿<?php  
+
+// Llamando a los campos
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+$mensaje = $_POST['mensaje'];
+
+// Datos para el correo
+$destinatario = "dvargas@grupoptm.com";
+$asunto = "Contacto desde nuestra web";
+
+$carta = "De: $nombre \n";
+$carta .= "Correo: $correo \n";
+$carta .= "Mensaje: $mensaje";
+
+
+//echo $nombre;
+// Enviando Mensaje
+mail($destinatario, $asunto, $carta);
+ 
+//echo('<script language="JavaScript>alert("Tu información se envio con exito");</script>');
+?>
